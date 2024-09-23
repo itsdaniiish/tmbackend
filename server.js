65d4f17040dace12e2492b652c001e773dc568bf
Danish <speak2danishofficially.com>
2024-09-23 16:11:37 +0530

    updated

--- server.js
+++ server.js
@@ -12,13 +12,10 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 // CORS configuration
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000", // Use environment variable or fallback to localhost
-  credentials: true,
-};
+
 
 // Middleware setup
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
